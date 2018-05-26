d19ee633b95334a9879603e0926e67c513d11260
Werner de Groot <github@wernerdegroot.nl>
2018-05-26 14:02:41 +0200

    Move away from `Advice`, but let the user manually declare an alternative (`Alternative`).

--- AsyncResult.ts
+++ AsyncResult.ts
@@ -1,5 +1,5 @@
 import { flatten } from './utils'
-import { ADVICE, AWAITING_RESULT, REQUEST_CANCELLED, RESULT_EXPIRED, RESULT_RECEIVED } from './consts'
+import { AWAITING_RESULT, REQUEST_CANCELLED, RESULT_EXPIRED, RESULT_RECEIVED } from './consts'
 import * as CacheItem from './CacheItem'
 import * as RequestState from './RequestState'
 import { RESULT_ARRIVED } from './old/Action'
@@ -7,7 +7,6 @@ import { RESULT_ARRIVED } from './old/Action'
 export type AsyncResult<Result>
   = AwaitingResult<Result>
   | ResultReceived<Result>
-  | Advice<Result>
 
 export class AwaitingResult<Result> {
   public readonly type: 'AWAITING_RESULT' = AWAITING_RESULT
@@ -28,37 +27,7 @@ export class ResultReceived<Result> {
   }
 }
 
-export class Advice<Result> {
-  public readonly type: 'ADVICE' = ADVICE
-
-  constructor(public readonly previousResults: Result[]) {
-  }
-}
-
-export interface Factory<Result> {
-
-  awaitingResult(previousResults: Result[]): AwaitingResult<Result>
-
-  resultReceived(result: Result, previousResults: Result[]): ResultReceived<Result>
-
-  advice(previousResults: Result[]): Advice<Result>
-}
-
-export function defaultFactory<Result>(): Factory<Result> {
-  return {
-    awaitingResult(previousResults: Result[]): AwaitingResult<Result> {
-      return new AwaitingResult(previousResults)
-    },
-    resultReceived(result: Result, previousResults: Result[]): ResultReceived<Result> {
-      return new ResultReceived(result, previousResults)
-    },
-    advice(previousResults: Result[]): Advice<Result> {
-      return new Advice<Result>(previousResults)
-    }
-  }
-}
-
-export function forKey<Key, Result>(cacheItems: CacheItem.CacheItem<Key, Result>[], key: Key, keysAreEqual: (left: Key, right: Key) => boolean, factory: Factory<Result>): AsyncResult<Result> {
+export function forKey<Key, Result, Alternative>(cacheItems: CacheItem.CacheItem<Key, Result>[], key: Key, keysAreEqual: (left: Key, right: Key) => boolean, alternative: Alternative): AsyncResult<Result> | Alternative {
   const cacheItemsForKey = cacheItems
     .filter(cacheItem => keysAreEqual(cacheItem.key, key))
     .sort(CacheItem.ordering)
@@ -74,64 +43,17 @@ export function forKey<Key, Result>(cacheItems: CacheItem.CacheItem<Key, Result>
   }, [])
 
   if (head === undefined || head.requestState.type === REQUEST_CANCELLED) {
-    return factory.advice(previousResults)
+    return alternative
   } else if (head.requestState.type === RESULT_EXPIRED) {
-    return factory.advice([head.requestState.result, ...previousResults])
+    return alternative
   } else {
     if (head.requestState.type === AWAITING_RESULT) {
-      return factory.awaitingResult(previousResults)
+      return new AwaitingResult(previousResults)
     } else if (head.requestState.type === RESULT_RECEIVED) {
-      return factory.resultReceived(head.requestState.result, previousResults)
+      return new ResultReceived(head.requestState.result, previousResults)
     } else {
       const exhaustive: never = head.requestState
       throw new Error(exhaustive)
     }
-  }
-}
-
-export function map<A, B>(asyncResult: AsyncResult<A>, fn: (a: A) => B): AsyncResult<B> {
-  const mappedPreviousResults = asyncResult.previousResults.map(fn)
-  switch (asyncResult.type) {
-    case AWAITING_RESULT:
-      return new AwaitingResult(mappedPreviousResults)
-    case RESULT_RECEIVED:
-      return new ResultReceived(fn(asyncResult.result), mappedPreviousResults)
-    case ADVICE:
-      return new Advice(mappedPreviousResults)
-    default:
-      const exhaustive: never = asyncResult
-      throw new Error(exhaustive)
-  }
-}
-
-export function flatMap<A, B>(asyncResult: AsyncResult<A>, fn: (a: A) => AsyncResult<B>, factory: Factory<B>): AsyncResult<B> {
-  const flatMappedPreviousResults = flatten(asyncResult.previousResults.map(fn).map(ar =>
-    ar.type === RESULT_RECEIVED
-      ? [ar.result, ...ar.previousResults]
-      : ar.previousResults
-  ))
-  switch (asyncResult.type) {
-    case AWAITING_RESULT:
-      return factory.awaitingResult(flatMappedPreviousResults)
-    case RESULT_RECEIVED: {
-      const flatMapped = fn(asyncResult.result)
-      const previousResults = flatMapped.previousResults.concat(flatMappedPreviousResults)
-      switch (flatMapped.type) {
-        case AWAITING_RESULT:
-          return factory.awaitingResult(previousResults)
-        case RESULT_RECEIVED:
-          return factory.resultReceived(flatMapped.result, previousResults)
-        case ADVICE:
-          return factory.advice(previousResults)
-        default:
-          const exhaustive: never = flatMapped
-          throw new Error(exhaustive)
-      }
-    }
-    case ADVICE:
-      return factory.advice(flatMappedPreviousResults)
-    default:
-      const exhaustive: never = asyncResult
-      throw new Error(exhaustive)
   }
 }
